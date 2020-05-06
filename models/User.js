bd01deaf298ad99827bf3ccd5f18816acb4dccc5
Nipul Singal <nipul00rock@gmail.com>
2020-05-06 16:34:07 +0530

    api saved

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// models/User.js
+var mongoose = require('mongoose');
+const UserSchema = mongoose.Schema({
+   firstname:{
+     type:String,
+     require:true
+   },
+   lastname:{
+    type:String,
+    require:true
+  },
+  telnum:{
+    type:String,
+    require:true
+  },
+  email:{
+    type:String,
+    require:true
+  },
+   password:{
+     type:String,
+     require:true
+   }
+});
+module.exports = User = mongoose.model('UserSchema',UserSchema);
---
+++ mysetup/myurl.js
@@ -0,0 +1,5 @@
+// mysetup/myurl.js
+module.exports = {
+    myurl: 'mongodb://localhost:27017/Users',
+    secret: 'mysecretkey'  //going to be used later on for key token
+ }
